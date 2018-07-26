659238a103ed41362955854d2181dc5927f46115
Coder0418 <70669641@qq.com>
2018-07-26 15:46:02 +0800

    教师新增getter/setter

--- src/com/project/Teacher.java
+++ src/com/project/Teacher.java
@@ -5,4 +5,22 @@ public class Teacher {
 	private Integer studentId;
 	private String studentName;
 	
+	public Integer getStudentId() {
+		return studentId;
+	}
+	
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+	
+	
 }
